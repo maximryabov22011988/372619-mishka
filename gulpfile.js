ad208050e96e7cdfbe76f2642bd759a9d7a77d03
Maxim Ryabov <designmr2015@gmail.com>
2018-02-14 22:06:55 +0400

    Создает сборку для разработки

--- gulpfile.js
+++ gulpfile.js
@@ -20,18 +20,14 @@ var del = require("del");                          // удаляет папки,
 var run = require("run-sequence");                 // выполняет последовательность задач Gulp в указанном порядке
 var server = require("browser-sync").create();     // запускает локальный сервер
 
-
 /* Минифицирует HTML файлы в папке build*/
-
 gulp.task("html", function() {
   return gulp.src("build/*.html")
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest("build"));
 });
 
-
 /* Готовит CSS для build версии */
-
 gulp.task("style", function() {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -47,9 +43,7 @@ gulp.task("style", function() {
     .pipe(gulp.dest("build/css"));
 });
 
-
 /* Проверяет, объединяет, минифицирует JS для build версии */
-
 gulp.task("scripts", function() {
   return gulp.src("source/js/*.js")
     .pipe(plumber())
@@ -62,9 +56,7 @@ gulp.task("scripts", function() {
     .pipe(gulp.dest("build/js"));
 });
 
-
 /* Оптимизирует изображения */
-
 gulp.task("images", function() {
   return gulp.src("build/img/*.{png,jpg,svg}")
     .pipe(imagemin([
@@ -75,18 +67,15 @@ gulp.task("images", function() {
     .pipe(gulp.dest("build/img"));
 });
 
-
-/* Конвертирует контентные изображения в формат webP */
-
+/* Копирует контентные изображения и конвертирует в формат webP в build версии */
 gulp.task("webp", function() {
-  return gulp.src("build/img/**/*.{png,jpg}")
+  return gulp.src("source/img/content-image/*.{png,jpg}")
+    .pipe(gulp.dest("build/img/"))
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("build/img/"));
 });
 
-
 /* Собирает SVG спрайт */
-
 gulp.task("sprite", function() {
   return gulp.src("source/img/icons-for-sprite/**")
     .pipe(svgmin())
@@ -97,9 +86,7 @@ gulp.task("sprite", function() {
     .pipe(gulp.dest("build/img"));
 });
 
-
-/* Запускает локальный сервер */
-
+/* Запускает локальный сервер для build версии */
 gulp.task("serve", function() {
   server.init({
     server: "build/"
@@ -108,9 +95,7 @@ gulp.task("serve", function() {
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
 });
 
-
 /* Копирует файлы */
-
 gulp.task("copy", function() {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
@@ -124,16 +109,12 @@ gulp.task("copy", function() {
     .pipe(gulp.dest("build"));
 });
 
-
 /* Удаляет папку build и все ее содержимое */
-
 gulp.task("clean", function() {
   return del("build");
 });
 
-
-/* Запускает построение build версии сайта */
-
+/* Запускает сборку build версии */
 gulp.task("build", function(done) {
   run(
     "clean",
@@ -145,6 +126,53 @@ gulp.task("build", function(done) {
     "webp",
     "sprite",
     "serve",
+    done
+  );
+});
+
+
+/* Готовит CSS для develop версии */
+gulp.task("style:develop", function() {
+  gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+/* Копирует контентные изображения и конвертирует в формат webP в develop версии */
+gulp.task("webp:develop", function() {
+  return gulp.src("source/img/content-image/*.{png,jpg}")
+    .pipe(gulp.dest("source/img/"))
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img/"));
+});
+
+/* Запускает локальный сервер для develop версии*/
+gulp.task("serve:develop", function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", ["style:develop"]);
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+/* Запускает develop версию для разработки */
+gulp.task("develop", function(done) {
+  run(
+    "style:develop",
+    //"webp:develop",
+    "serve:develop",
     done
   );
 });
